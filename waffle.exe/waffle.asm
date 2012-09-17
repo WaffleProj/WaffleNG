64edaee8a3265384f6fdc6c187546967998a2e27
MakiseKurisu <269909597@qq.com>
2012-09-16 21:32:23 -0500

    JWasm Bug. Stop
    https://sourceforge.net/tracker/index.php?func=detail&aid=3568271&group_id=255677&atid=1126895#

--- waffle.exe/waffle.asm
+++ waffle.exe/waffle.asm
@@ -4,40 +4,45 @@
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; Include 文件定义
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+UNICODE		equ	TRUE
+
 include		windows.inc
 includelib	kernel32.lib
+includelib	user32.lib
 
-include		Unicode.inc
-include		Strings.mac
-UNICODE		equ	TRUE
+include		Console.inc
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 数据段
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>		
 		.data?
 hStdIn		dd	?	;控制台输入句柄
 hStdOut		dd	?	;控制台输出句柄
+hStdErr		dd	?	;控制台错误句柄
 dwBytesRead	dd	?
 dwBytesWrite	dd	?
-dwOutPut	dd	?
+
+uszBuf		dw	100 dup (?)
 		.const
-sz0Dh		db	0dh
+uszTitle	dw	"Waffle v0.10",0
+usz0Dh		dw	0dh
+uszBOM		dw	0FEFFh
+uszTest		dw	"ABC",0
+uszTest2	dw	"DEF",0
+uszFmt		dw	"%08X",0
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 代码段
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 		.code
-include		CmdLine.asm
+include		_CmdLine.asm
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 start:
-		invoke	GetStdHandle,STD_INPUT_HANDLE
-		mov	hStdIn,eax
-		invoke	GetStdHandle,STD_OUTPUT_HANDLE
-		mov	hStdOut,eax
+		invoke	_SetConsole,addr uszTitle
 lop:
-		invoke	WriteFile,hStdOut,addr sz0Dh,1,addr dwBytesWrite,NULL
-		invoke	WriteFile,hStdOut,$CTA0('测试'),4,addr dwBytesWrite,NULL
+		Cout	offset uszTest,3
+		Cout	offset usz0Dh,1
 		invoke	Sleep,1000
-		invoke	WriteFile,hStdOut,addr sz0Dh,1,addr dwBytesWrite,NULL
-		invoke	WriteFile,hStdOut,$CTA0('二次'),4,addr dwBytesWrite,NULL
+		Cout	offset uszTest2,3
+		Cout	offset usz0Dh,1
 		invoke	Sleep,1000
 		jmp	lop
 		invoke	Sleep,5000
