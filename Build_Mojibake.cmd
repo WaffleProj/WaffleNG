ee3c98ff8c76162cbb7fa03776ef4aa86e552a14
MakiseKurisu <saberconer@gmail.com>
2013-07-21 18:30:49 -0500

    Link against common10.dll
    Avoid dll hell

--- Build_Mojibake.cmd
+++ Build_Mojibake.cmd
@@ -9,10 +9,12 @@ call	Common	Compile	detour.c
 call	Common	Compile	init.c
 call	Common	Compile	src\exception\exception.c
 if	"%Machine%" == "I386"	(
-ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\Component\Waffle\%Machine% --enable-stdcall-fixup -kill-at -e _DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lWaffle_common -lmingwex -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi
+ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\Component\Waffle\%Machine% --enable-stdcall-fixup -kill-at -e _DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lWaffle_common10 -lmingwex -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi
 	)
 if	"%Machine%" == "AMD64"	(
-ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\Component\Waffle\%Machine% --enable-stdcall-fixup -kill-at -e  DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lWaffle_common -lmingwex -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi
+ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\Component\Waffle\%Machine% --enable-stdcall-fixup -kill-at -e  DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lWaffle_common10 -lmingwex -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi
 	)
 call	Common	CleanUp
 :nocore
+
+popd
