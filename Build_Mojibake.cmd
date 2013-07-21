e71ef5f4a40e38b0480aa4e5c5a0c7b7e19b71ec
MakiseKurisu <saberconer@gmail.com>
2013-07-20 21:36:18 -0500

    Remove mojibake.bak since main.dll works well

--- Build_Mojibake.cmd
+++ Build_Mojibake.cmd
@@ -9,10 +9,10 @@ call	Common	Compile	detour.c
 call	Common	Compile	init.c
 call	Common	Compile	src\exception\exception.c
 if	"%Machine%" == "I386"	(
-ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\SDK\Lib\%Machine% --enable-stdcall-fixup -kill-at -e _DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi -lWaffle_common -lmingwex
+ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\Component\Waffle\%Machine% --enable-stdcall-fixup -kill-at -e _DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lWaffle_common -lmingwex -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi
 	)
 if	"%Machine%" == "AMD64"	(
-ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\SDK\Lib\%Machine% --enable-stdcall-fixup -kill-at -e  DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi -lWaffle_common -lmingwex
+ld	--subsystem windows --dll -L%MinGW%\lib -L%OUTPUT_PATH%\Component\Waffle\%Machine% --enable-stdcall-fixup -kill-at -e  DllMain -o %OUTPUT_PATH%\Component\%Project%\%Machine%\%Project%_main.dll mojibake.o detour.o init.o exception.o -lWaffle_common -lmingwex -lkernel32 -luser32 -lgdi32 -lpsapi -lshlwapi
 	)
 call	Common	CleanUp
 :nocore
