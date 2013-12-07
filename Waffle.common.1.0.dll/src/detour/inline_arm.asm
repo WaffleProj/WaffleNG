501cf5d8d96749fbfc0ba1a29b541d69ff64ce00
MakiseKurisu <saberconer@gmail.com>
2013-12-06 23:22:36 -0600

    Update ARM Assembly

---
+++ Waffle.common.1.0.dll/src/detour/inline_arm.asm
@@ -0,0 +1,25 @@
+        area	.text, code
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+WaffleGetCallersAddress	proc
+		export	WaffleGetCallersAddress
+		mov	r0,0
+		mov	r1,0
+		mov	pc,lr
+WaffleGetCallersAddress	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+WaffleInlineHandler	proc
+		export	WaffleInlineHandler
+		bkpt	0
+		mov	r0,0
+		mov	r1,0
+		mov	pc,lr
+WaffleInlineHandler	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+WaffleInlineDetour	proc
+		export	WaffleInlineDetour
+		mov	r0,0
+		mov	r1,0
+		mov	pc,lr
+WaffleInlineDetour	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		end
